ef9816be227d1e2314cb1dc8c7c3982bce3a9daf
aarthiap3107 <aarthzzz3107@gmail.com>
2026-07-01 19:03:33 +0530

    vault(easy): 1. Two Sum [Array, Hash Table]

--- Easy/1_Two_Sum.java
+++ Easy/1_Two_Sum.java
@@ -1,20 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int left=0;
-        int right=nums.length-1;
-        while(left<right){
-            int curr=nums[left]+nums[right];
-            if(curr==target){
-                return new int[]{left,right};
+        HashMap<Integer,Integer>map=new HashMap();
+        for(int i=0;i<nums.length;i++){
+            int complement=target-nums[i];
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement),i};
             }
-            if(curr>target){
-                right--;
-            }
-            if(curr<target){
-                left++;
-            }
+            map.put(nums[i],i);
         }
         return new int[]{};
-        
     }
 }
