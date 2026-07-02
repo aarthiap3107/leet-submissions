a184f5d9c459fec4a6236d1d3665effbc0278da6
aarthiap3107 <aarthzzz3107@gmail.com>
2026-07-02 23:40:55 +0530

    vault(easy): 338. Counting Bits [Dynamic Programming, Bit Manipulation]

--- Easy/338_Counting_Bits.java
+++ Easy/338_Counting_Bits.java
@@ -1,12 +1,12 @@
 class Solution {
     public int[] countBits(int n) {
-        int result[]=new int[n+1];
-        for(int i=1;i<=n;i++){
-            int num=i;
-            while(num!=0){
+        int result[] = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            int num = i;
+            while (num != 0) {
                 result[i]++;
-                num&=num-1;
-            }    
+                num &= num - 1;
+            }
         }
         return result;
     }
